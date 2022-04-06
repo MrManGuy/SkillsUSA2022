e7c6d73dd5ae6ef5a107e9105b6043f5375775d4
MrManGuy <rinicholas99@gmail.com>
2022-04-06 10:15:45 -0500

    Completed Project 1
    Added Project 1 tests powerpoint and finished the first project.

--- project1.py
+++ project1.py
@@ -1,10 +1,9 @@
-from logging import PlaceHolder
-from struct import calcsize
 from tkinter import *
 from tkinter import messagebox
+from turtle import width
 
-window = Tk()
-window.title("Food Truck - 1155")
+register = Tk()
+register.title("Register - 1155")
 all_orders = []
 
 #Input: value of the box that's being edited
@@ -12,26 +11,26 @@ all_orders = []
 def checkIfInteger(value):
     if value.isnumeric() or value == "":
         return True
-    messagebox.showwarning(title=None, message="Quantity must be an integer!")
+    messagebox.showwarning(title=None, message=value + " is invalid\nQuantity must be an integer!")
     return False
 
 #Input: All int values of number of items purchased
 #Returns an array with costs of each item for the order
-def calculateCosts(dogs, brats, burgers, fries, sodas, waters):
-    return [(dogs, (dogs*2.50)), (brats, (brats*3.50)), (burgers, (burgers*5.0)), (fries, (fries*2.0)), (sodas, (sodas*2.0)), (waters, (waters*0.0))]
+def calculateCosts(values):
+    return [[values[0], (values[0]*2.50)], [values[1], (values[1]*3.50)], [values[2], (values[2]*5.0)], [values[3], (values[3]*2.0)], [values[4], (values[4]*2.0)], [values[5], (values[5]*0.0)]]
 
 #Input: All stringvar versions of number of items purchased
-def processPurchase(dogs, brats, burgers, fries, sodas, waters):
-    dogs = int(dogs.get())
-    brats = int(brats.get())
-    burgers = int(burgers.get())
-    fries = int(fries.get())
-    sodas = int(sodas.get())
-    waters = int(waters.get())
-    order = calculateCosts(dogs, brats, burgers, fries, sodas, waters)
+#Appends each days total to the all_orders array which is used to create the totals segment of the window
+def processPurchase(values):
+    for i, value in enumerate(values):
+        if value.get().isnumeric() == False:
+            values[i] = 0
+        else:
+            values[i] = int(value.get())
+    order = calculateCosts(values)
     all_orders.append(order)
     print(order)
-    createMainScreen()
+    createRegisterScreen()
 
 #Input: All string vars
 #Output: None
@@ -44,26 +43,62 @@ def resetStringVars(dogs, brats, burgers, fries, sodas, waters):
     sodas.set("0")
     waters.set("0")
 
+#Creates the screen which will display totals
+def createTotals():
+    if all_orders == []:
+        return
+    else:
+        #Loops over all items in all orders across all days an totals the values.
+        condensed_orders = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+        for day in all_orders:
+            for pos, item in enumerate(day):
+                condensed_orders[pos][0] += item[0]
+                condensed_orders[pos][1] += item[1]
+
+        lbl_spacer = Label(text="\t")
+        lbl_spacer.grid(column=2, row=0)
+
+        lbl_hot_dogs = Label(text="Hot Dogs: " + str(condensed_orders[0][0]) + " - Total Cost: $" + str(condensed_orders[0][1]))
+        lbl_hot_dogs.grid(column=3, row=0, sticky="W")
+
+        lbl_brats = Label(text="Brats: " + str(condensed_orders[1][0]) + " - Total Cost: $" + str(condensed_orders[1][1]))
+        lbl_brats.grid(column=3, row=1, sticky="W")
+
+        lbl_burgers = Label(text="Hamburgers: " + str(condensed_orders[2][0]) + " - Total Cost: $" + str(condensed_orders[2][1]))
+        lbl_burgers.grid(column=3, row=2, sticky="W")
+
+        lbl_fries = Label(text="Fries: " + str(condensed_orders[3][0]) + " - Total Cost: $" + str(condensed_orders[3][1]))
+        lbl_fries.grid(column=3, row=3, sticky="W")
+
+        lbl_sodas = Label(text="Soda: " + str(condensed_orders[4][0]) + " - Total Cost: $" + str(condensed_orders[4][1]))
+        lbl_sodas.grid(column=3, row=4, sticky="W")
+
+        lbl_waters = Label(text="Water: " + str(condensed_orders[5][0]) + " - Total Cost: $" + str(condensed_orders[5][1]))
+        lbl_waters.grid(column=3, row=5, sticky="W")
+        
+        lbl_total = Label(text="Total Cost: $" + str(sum([condensed_orders[i][1] for i in range(6)])))
+        lbl_total.grid(column=3, row=6, sticky="W")
+
 
 #Creates the main GUI for the user
-def createMainScreen():
+def createRegisterScreen():
     lbl_hot_dogs = Label(text="Hot dogs - $2.50: ")
-    lbl_hot_dogs.grid(column=0, row=0)
+    lbl_hot_dogs.grid(column=0, row=0, sticky="W")
 
     lbl_brats = Label(text="Brats - $3.50: ")
-    lbl_brats.grid(column=0, row=1)
+    lbl_brats.grid(column=0, row=1, sticky="W")
 
     lbl_burgers = Label(text="Hamburgers - $5.00: ")
-    lbl_burgers.grid(column=0, row=2)
+    lbl_burgers.grid(column=0, row=2, sticky="W")
 
     lbl_fries = Label(text="Fries - $2.00: ")
-    lbl_fries.grid(column=0, row=3)
+    lbl_fries.grid(column=0, row=3, sticky="W")
 
     lbl_sodas = Label(text="Soda - $2.00: ")
-    lbl_sodas.grid(column=0, row=4)
+    lbl_sodas.grid(column=0, row=4, sticky="W")
 
     lbl_waters = Label(text="Water - Free: ")
-    lbl_waters.grid(column=0, row=5)
+    lbl_waters.grid(column=0, row=5, sticky="W")
 
     hot_dogs = StringVar(value="0")
     inp_hot_dogs = Entry(validate='all', textvariable=hot_dogs)
@@ -95,18 +130,20 @@ def createMainScreen():
     inp_waters["validatecommand"] = (inp_waters.register(checkIfInteger),'%P')
     inp_waters.grid(column=1, row=5)
 
-    btn_calculate = Button(text="Calculate", command= lambda: processPurchase(hot_dogs, brats, burgers, fries, sodas, waters))
-    btn_calculate.grid(column=0, row=6, columnspan=2, sticky="NWES")
+    createTotals()
+
+    btn_calculate = Button(text="Calculate", command= lambda: processPurchase([hot_dogs, brats, burgers, fries, sodas, waters]))
+    btn_calculate.grid(column=0, row=7, columnspan=5, sticky="NWES")
 
     btn_clear = Button(text="Clear", command=lambda: resetStringVars(hot_dogs, brats, burgers, fries, sodas, waters))
-    btn_clear.grid(column=0, row=7, columnspan=2, sticky="NWES")
+    btn_clear.grid(column=0, row=8, columnspan=5, sticky="NWES")
 
     btn_exit = Button(text="Exit", command=exit)
-    btn_exit.grid(column=0, row=8, columnspan=2, sticky="NWES")
+    btn_exit.grid(column=0, row=9, columnspan=5, sticky="NWES")
 
 def main():
-    createMainScreen()
-    window.mainloop()
+    createRegisterScreen()
+    register.mainloop()
 
 if __name__ == "__main__":
     main()
